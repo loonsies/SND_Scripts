56215a6e5953c1fea350cc7aff16940a2492363c
senolem <37041587+senolem@users.noreply.github.com>
2025-04-28 06:05:27 +0200

    bech

---
+++ getonthemountbitch.lua
@@ -0,0 +1,9 @@
+yield('/target Luna Tombeneige')
+if (not HasTarget() or GetTargetName() ~= 'Luna Tombeneige') then
+  return
+end
+yield('/wait 0.5')
+yield('/follow <t>')
+while not (GetCharacterCondition(10)) do
+  yield('/snd run vroom2')
+end
